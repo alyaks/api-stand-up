8483f7632925ed6972bc46006e381c3878f029e9
Alyaksandr Pankratau <alyaksandrpankratau@gmail.com>
2024-07-16 19:25:50 +0300

    feat: add headers

--- modules/handleClientRequest.js
+++ modules/handleClientRequest.js
@@ -3,19 +3,19 @@ import fs from "node:fs/promises";
 import { CLIENTS } from "../index.js";
 
 export const handleClientRequest = async (req, res, ticketNumber) => {
-    try {
-        const clientData = await fs.readFile(CLIENTS, 'utf-8')
-        const clients = JSON.parse(clientData)
-        const client = clients.find(c => c.ticketNumber === ticketNumber)
-        
-        if (!client) {
-            sendError(res, 404, "Not found")
-            return
-        }
+  try {
+    const clientData = await fs.readFile(CLIENTS, "utf-8");
+    const clients = JSON.parse(clientData);
+    const client = clients.find((c) => c.ticketNumber === ticketNumber);
 
-        sendData(res, client)
-    } catch (error) {
-        console.log(`error: ${error}`)
-        sendError(res, 500, `Server is error: ${error}`)
+    if (!client) {
+      sendError(res, 404, "Not found");
+      return;
     }
-}
+
+    sendData(res, client);
+  } catch (error) {
+    console.log(`error: ${error}`);
+    sendError(res, 500, `Server is error: ${error}`);
+  }
+};
